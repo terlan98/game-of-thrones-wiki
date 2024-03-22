79d7004810936d629a56072cf8cfc7f743988cb7
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2024-03-22 22:25:44 +0400

    Initial Commit

---
+++ GameOfThronesWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameOfThronesWiki
+//
+//  Created by Tarlan Ismayilsoy on 22.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameOfThronesWiki/GameOfThronesWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesWikiApp.swift
+//  GameOfThronesWiki
+//
+//  Created by Tarlan Ismayilsoy on 22.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
